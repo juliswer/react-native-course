0508f2c7af6948d8a8d49e49dc7b929ad8d9ff93
Julian Swerdlin <julianswer36@gmail.com>
2021-07-11 20:11:45 -0300

    uploading new files

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello world!</Text>
     </View>
-  ); 
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container: {flex: 1, justifyContent: "center", alignItems: "center"},
+  title: {fontSize: 20}
+})
+
+export default App
