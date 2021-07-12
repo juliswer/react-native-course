d13aa08e04d7c5bd8dfd97836fb09ae9fec4f7f3
Julian Swerdlin <julianswer36@gmail.com>
2021-07-12 13:57:51 -0300

    new files

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native'
+import {Text, View, StyleSheet, Image} from 'react-native'
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello world!</Text>
+      <Image 
+        source={{uri: 'https://picsum.photos/200/300'}}
+        style={styles.image}
+      />
     </View>
   )
 }
@@ -13,10 +17,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, 
     justifyContent: "center", 
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#292929"
   },
   title: {
-    fontSize: 20
+    fontSize: 30,
+    color: "#fff"
+  },
+  image: {
+    height: 200,
+    width: 200
   }
 })
 
